cf397692201c7e82ee95fe07548a626f8f2551d1
TaiWowAde <twin.z@hotmail.co.uk>
2025-08-31 21:07:59 +0100

    changed from token to basic auth

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
