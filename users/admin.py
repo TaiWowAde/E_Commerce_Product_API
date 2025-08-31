ad74a6ca1c0bf7e52f705a12082ad41d4ac653e0
TaiWowAde <twin.z@hotmail.co.uk>
2025-08-31 21:30:27 +0100

    Improve user admin configuration

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
+
 from .models import User
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "username", "email", "is_active", "date_joined")
-    search_fields = ("username", "email")
+    """Configuration for the custom :class:`User` model in Django admin."""
+
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "is_active",
+        "is_staff",
+        "date_joined",
+    )
+    search_fields = ("username", "email", "first_name", "last_name")
+    list_filter = ("is_active", "is_staff", "is_superuser", "date_joined")
+    ordering = ("id",)
+    readonly_fields = ("date_joined", "last_login")
+
